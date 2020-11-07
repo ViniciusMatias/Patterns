ba5de3217fae01c8bbeb5515687efaabfa4dab42
Vinicius Matias <matias.uplink@outlook.com>
2020-11-07 18:18:23 -0300

    Extendendo Jframe

--- src/Criacao/Singleton/Janela.java
+++ src/Criacao/Singleton/Janela.java
@@ -1,2 +1,20 @@
-package Criacao.Singleton;public class Janela {
-}
+package Criacao.Singleton;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Janela extends JFrame {
+
+
+    private static final Janela INSTANCE = new Janela();
+
+    private Janela(){
+        setPreferredSize(new Dimension(640,480));
+    }
+
+    public static Janela getInstace(){
+       return INSTANCE;
+    }
+
+    }
+
